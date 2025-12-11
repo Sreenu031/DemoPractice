e5c5e760c99fbb38ee0f6b7a131c8f0fc5ae5445
Medisetti Srinu <108221206+Sreenu031@users.noreply.github.com>
2025-12-11 16:37:46 +0530

    Add additional log message to script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("hello wrold");
 console.log("hello main branch");
 
 console.log("this is adding form the github");
+
+console.log("this is also another line i am adding using github editor");
